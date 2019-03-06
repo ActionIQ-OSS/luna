4ede63761f416149b115337598f944b9fd4ed262
Larry Finn <larry@actioniq.co>
2019-03-06 16:43:08 -0500

    comment

--- src/scala/co/actioniq/luna/dao/DAOMySQLProfile.scala
+++ src/scala/co/actioniq/luna/dao/DAOMySQLProfile.scala
@@ -9,12 +9,14 @@ trait DAOMySQLProfile extends MySQLProfile {
 
 object DAOMySQLProfile extends DAOMySQLProfile {
   import slick.ast._
+  //Copied from MySQLProfile
   final case class RowNum(sym: AnonSymbol, inc: Boolean) extends NullaryNode with SimplyTypedNode {
     type Self = RowNum
     def buildType = ScalaBaseType.longType
     def rebuild = copy()
   }
 
+  //Copied from MySQLProfile
   final case class RowNumGen(sym: AnonSymbol, init: Long) extends NullaryNode with SimplyTypedNode {
     type Self = RowNumGen
     def buildType = ScalaBaseType.longType
